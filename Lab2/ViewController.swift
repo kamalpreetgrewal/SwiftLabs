8c3537e9c526bc6654787c02af49cd5a44359bff
kamalpreetgrewal <grewalkamal005@gmail.com>
2018-01-30 13:55:17 -0500

    Complete start, stop, resume, reset functionality of the timer

--- Lab2/ViewController.swift
+++ Lab2/ViewController.swift
@@ -9,16 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()
+    var isTimerRunning = false
+    var time = 120
+    var currentCount = 0
 
     @IBOutlet weak var label: UILabel!
     
+    @IBOutlet weak var startButton: UIButton!
+    
     @IBAction func start(_ sender: UIButton) {
+        run()
+        isTimerRunning = true
+        sender.isEnabled = false
+    }
+    
+    func run(){
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateView), userInfo: nil, repeats: true)
+    }
+    
+    func updateView(){
+        currentCount += 1
+        label.text = String(currentCount)
     }
     
     @IBAction func pause(_ sender: UIButton) {
+        if(isTimerRunning) {
+            timer.invalidate()
+            sender.setTitle("Resume", for: .normal)
+            isTimerRunning = false
+        } else{
+            sender.setTitle("Pause", for: .normal)
+            run()
+            isTimerRunning = true
+        }
     }
     
     @IBAction func reset(_ sender: UIButton) {
+        isTimerRunning = false
+        currentCount = 0
+        label.text = String(currentCount)
+        timer.invalidate()
+        startButton.isEnabled = true
     }
     
     override func viewDidLoad() {
