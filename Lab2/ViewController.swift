85b6496cdfaa9289285b4d2d8fc9556bbdb30e6e
h18 <h18@ny-imac-19.local>
2018-01-30 10:52:41 -0500

    Initial Commit

---
+++ Lab2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab2
+//
+//  Created by h18 on 2018-01-30.
+//  Copyright © 2018 h18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
